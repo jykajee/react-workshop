decb62207669e897ee44b3ec1d0b72241dce658d
Jyri Makinen <jyri247@gmail.com>
2018-03-17 01:30:29 +0200

    del nvmrc

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-import Card from '../components/Card';
+import Card from '../components/card';
 
 const CardGrid = styled.div`
     display: grid;
